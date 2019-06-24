ee1e750e8a57bf2fc86f7623032f4ccfc45b1679
Karan0811 <46247163+Karan0811@users.noreply.github.com>
2019-06-24 15:42:51 +0530

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -19,7 +19,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name="index"),
-    path('analyse/', views.analyse, name="analyse"),
-
+    path('', views.index, name='index'),
+    path('analyze', views.analyze, name='analyze'),
+    path('about', views.about, name='about')
 ]
